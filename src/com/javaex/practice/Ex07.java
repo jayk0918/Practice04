7c15199519346a21eee6f6f8745dfca536d726ec
Mocking_jayk0918 <jesper2able@gmail.com>
2022-04-21 08:21:44 +0900

    1.0(java_practice_Q08remained 22.04.21 08:21)

--- src/com/javaex/practice/Ex07.java
+++ src/com/javaex/practice/Ex07.java
@@ -1,11 +1,16 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex07 {
 
 	public static void main(String[] args) {
 		
 		// 정수로 된 돈 액수를 입력
 		// 50000 ~ 1원 까지 각각 몇개로 나뉘는지
+		
+		Scanner sc = new Scanner(System.in);
+		
 		
 		int[]wonArray = new int[10];
 		wonArray[0] = 50000;
@@ -18,6 +23,20 @@ public class Ex07 {
 		wonArray[7] = 10;
 		wonArray[8] = 5;
 		wonArray[9] = 1;
+		
+		int[]count = new int[10];
+		
+		System.out.print("금액: ");
+		int money = sc.nextInt();
+		
+		for(int i=0; i<wonArray.length; i++) {
+			count[i] = money/wonArray[i];
+			money = money%wonArray[i]; // % 나머지 값을 하지 않으면 반복해서 count[i]값으로 나눔
+			System.out.println(wonArray[i] + "원:" + count[i] + "개");
+		}
+		
+		sc.close();
+		
 		
 		
 	}
--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -6,15 +6,27 @@ public class Ex08 {
 		// 1~45까지 임의의 숫자 6개를 출력하되 중복되지 않을 것
 		
 		int[] lotto = new int[6];
-		for(int i=0; i<lotto.length; i++) {
-			lotto[i] = (int)(Math.random()*45)+1;
-			
-			if(lotto[i] == lotto[i]) {
+		int duplicate = 0;	// 중복 체크용 변수
+		while(true) {
+			for(int i=0; i<lotto.length; i++) {
 				lotto[i] = (int)(Math.random()*45)+1;
+				for(int j=i+1; j<lotto.length-1; j++) {		// 중복 체크를 위해 배열 2개 운영
+					if(lotto[i+1]==lotto[j]) {
+						duplicate += 1;
+					}
+				}
+			}
+			if(duplicate == 0) {
+				break;
+			}else {
+				duplicate = 0;
+				continue;
 			}
 			
+		}
+		for (int i=0; i<lotto.length; i++) {
 			System.out.print(lotto[i] + " ");
 		}
-	}
-
-}
+		
+	}//main method end
+}//class end
