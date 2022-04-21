ac14cc3eff3383ef8dd525671a7d01e9acd408b7
Mocking_jayk0918 <jesper2able@gmail.com>
2022-04-21 18:26:31 +0900

    1.0(java_practice_selffeedback 22.04.21 18:26)

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -6,27 +6,29 @@ public class Ex08 {
 		// 1~45까지 임의의 숫자 6개를 출력하되 중복되지 않을 것
 		
 		int[] lotto = new int[6];
-		int duplicate = 0;	// 중복 체크용 변수
+		int duplicate = 0;	// 중복이 0일때만 반복문을 끝내도록 장치
+		
 		while(true) {
 			for(int i=0; i<lotto.length; i++) {
-				lotto[i] = (int)(Math.random()*45)+1;
-				for(int j=i+1; j<lotto.length-1; j++) {		// 중복 체크를 위해 배열 2개 운영
-					if(lotto[i+1]==lotto[j]) {
-						duplicate += 1;
+				lotto[i] = (int)(Math.random()*45)+1;	// 로또 번호 생성
+			}
+			
+			for(int i=0; i<lotto.length; i++) {		// i&j값 2개를 배열에 넣어 비교군 형성
+				for(int j=0; j<lotto.length; j++) {	
+					if(lotto[i]==lotto[j]) {
+						duplicate++;	// 중복시 정수값 +
 					}
 				}
 			}
-			if(duplicate == 0) {
-				break;
+			
+			if(duplicate==0) {
+				break;	// 중복값 0이 충족되면 break;
 			}else {
-				duplicate = 0;
+				duplicate = 0;	// 중복값이 존재해도 초기화해주지 않으면 무한루프, 중복값은 초기화가 필요함
 				continue;
 			}
 			
-		}
-		for (int i=0; i<lotto.length; i++) {
-			System.out.print(lotto[i] + " ");
-		}
+		}// while end
 		
 	}//main method end
 }//class end
